6f502bee4f7484fbff8223d85aa7101dd8864782
Luiz Fabricio <FabricioMacedo20>
2024-09-03 16:59:00 -0300

    atualizando repo

---
+++ Aulas_php/Dia2/4_estrutura_else_if/index.php
@@ -0,0 +1,69 @@
+<?php
+ 
+ if(10 >5 ) {
+
+    echo " entrou no if <br>";
+
+ } else if(10 > 6) {
+
+echo "entrou no else if <br>";
+
+ }
+
+ 
+ if(10 < 5 ) {
+
+    echo " entrou no if2 <br>";
+
+ } else if(10 > 6) {
+
+echo "entrou no else if2 <br>";
+
+ } else {
+    echo "entrou no else3 <br>";
+ }
+ 
+
+
+ if(10 < 5 ) {
+
+    echo " entrou no if4 <br>";
+
+ } else if(10 > 6) {
+
+echo "entrou no else if4 <br>";
+
+ } else if (11 > 1) {
+
+    echo "entrou no else if 2 4<br>";
+    
+ } else {
+    echo "entrou no else 4<br>";
+
+ }
+
+
+$a = 10;
+$b = 5;
+$msg = "entrou no else if";
+
+ if(10 < 5 ) {
+
+    echo " entrou no if 4 <br>";
+
+ } else if(10 > 6) {
+
+    echo $msg;
+    echo "<br>";
+
+echo "entrou no else if 4 <br>";
+
+ } else if (11 > 1) {
+
+    echo "entrou no else if 2 4<br>";
+    
+ } else {
+    echo "entrou no else 4<br>";
+
+ }
+ 
